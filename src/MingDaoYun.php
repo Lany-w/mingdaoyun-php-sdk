f18b5f3d16785ecc8e476102d14724a07af09fdb
Lany <wanglanyinn@163.com>
2021-12-29 17:16:59 +0800

    添加Exception,获取worksheet基础

--- src/MingDaoYun.php
+++ src/MingDaoYun.php
@@ -6,25 +6,49 @@
  */
 namespace Lany\MingDaoYun;
 
-use Lany\MingDaoYun\Traits\CallStatic;
+use Lany\MingDaoYun\Exceptions\Exception;
 
-class MingDaoYun
+/**
+ * Class MingDaoYun
+ * @package Lany\MingDaoYun
+ */
+class MingDaoYun extends Kernel
 {
-    use CallStatic;
-
+    //明道云APPKEY
     protected $appKey;
-
+    //明道云 sign
     protected $sign;
+    //明道云私有部署域名
+    protected $host;
+    //获取列表API
+    protected $getListApiV2 = '/api/v2/open/worksheet/getFilterRows';
 
     public function __construct()
     {
-
     }
 
-    protected function setUpMingDao($appKey, $sign)
+    /**
+     * Notes:明道云基础配置
+     * User: Lany
+     * DateTime: 2021/12/29 12:45 下午
+     * @param string $appKey
+     * @param string $sign
+     * @param string $host
+     * @return $this
+     */
+    public function setUpMingDao(string $appKey,string $sign,string $host)
     {
         $this->appKey = $appKey;
         $this->sign = $sign;
+        $this->host = $host;
 
+        return $this;
     }
+
+    public function get(string $worksheetId)
+    {
+        return $this->getList($worksheetId);
+    }
+
+
 }
