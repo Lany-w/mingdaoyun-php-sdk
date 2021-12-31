401cc2f2ae87818d566e5db86fe4272f3ab865c3
Todd <ghostlitao@gmail.com>
2021-12-31 08:43:37 +0800

    re structure tests

---
+++ tests/MingDaoYunFilterTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Notes:
+ * User: Lany
+ * DateTime: 2021/12/30 10:36 上午
+ */
+
+namespace Lany\MingDaoYun\Tests;
+
+use Lany\MingDaoYun\Exceptions\InvalidArgumentException;
+use Lany\MingDaoYun\Facade\MingDaoYun;
+use PHPUnit\Framework\TestCase;
+
+class MingDaoYunGetSortTest extends TestCase
+{
+    const appKey = '5922C61C66CF67AF';
+    const appSecret = 'OWI4M2EwOGFmNDZiMmI5YTAwN2IxMzYyYTNkYzQ1ZjJlYzYwMjUzY2VlYWYwYTlmYmIzMzJjN2ZjOGZlMDQ5NQ==';
+    const url = 'http://qtools.rinsys.com:80';
+    const workSheetTest = '60efbf797b786d8a492bfcee';
+
+
+    public function testWhere()
+    {
+        $mdy = MingDaoYun::setUpMingDao(self::appKey, self::appSecret, self::url);
+        $res = $mdy->limit(1)->get(self::workSheetTest);
+        $total = $res['data']['total'];
+        $count = $total;
+        $field = '60efbf797b786d8a492bfce7';
+        $res = $mdy->limit($count)->sort($field)->get(self::workSheetTest);
+        $this->assertArrayHasKey('data', $res);
+        $this->assertCount($count, $res['data']['rows']);
+
+        $data = $res['data'];
+        $firstData = $data['rows'][0];
+        $lastData = $data['rows'][$count - 1];
+        $this->assertGreaterThan(intval($firstData[$field]), intval($lastData[$field]));
+
+        $res = $mdy->limit($count)->sort($field, false)->get(self::workSheetTest);
+        $data = $res['data'];
+        $firstData = $data['rows'][0];
+        $lastData = $data['rows'][$count - 1];
+
+        $this->assertGreaterThan(intval($lastData[$field]), intval($firstData[$field]));
+
+    }
+
+}
