bcf3e7fe47e09134ed3717d270a10fbe76c989a2
Todd <todd@rinsys.com>
2021-12-31 11:16:00 +0800

    add return for facade

--- src/Facade/MingDaoYun.php
+++ src/Facade/MingDaoYun.php
@@ -10,7 +10,7 @@ namespace Lany\MingDaoYun\Facade;
 /**
  * Class MingDaoYun
  * @package Lany\MingDaoYun\Facade
- * @method \Lany\MingDaoYun\MingDaoYun init(string $appKey, string $sign, string $host)
+ * @method \Lany\MingDaoYun\MingDaoYun init(string $appKey, string $sign, string $host) static
  *
  */
 class MingDaoYun extends Facade
