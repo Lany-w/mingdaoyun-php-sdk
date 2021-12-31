a07a3bfe18440d1e8b9bbf1055f32c48d979ad6a
Lany <wlany@qq.com>
2021-12-31 15:11:57 +0800

    重构基类关系

---
+++ src/Facade/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Notes:
+ * User: Lany
+ * DateTime: 2021/12/29 3:58 下午
+ */
+
+namespace Lany\MingDaoYun\Facade;
+
+/**
+ * Class MingDaoYun
+ * @package Lany\MingDaoYun\Facade
+ * @method static setWorkSheetMap()
+ * @method static getList()
+ * @method static buildFilters($map, $condition='', $value='')
+ * @method static findOne()
+ * @method static getRelations()
+ */
+class Kernel extends Facade
+{
+    public static function getClass()
+    {
+        return 'kernel';
+    }
+}
