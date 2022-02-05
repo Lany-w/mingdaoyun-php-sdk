aa7d1a7518fb4238eb2bd15d5c6c0645572b56cb
Lany <wanglanyinn@163.com>
2022-02-05 14:36:38 +0800

    同步明道云数据 基础trait

--- src/Traits/BaseSyncTrait.php
+++ src/Traits/BaseSyncTrait.php
@@ -7,6 +7,8 @@
 namespace Lany\MingDaoYun\Traits;
 
 use Doctrine\Inflector\InflectorFactory;
+use Lany\MingDaoYun\Exceptions\Exception;
+use Lany\MingDaoYun\Filter;
 use Lany\MingDaoYun\MingDaoYun;
 
 trait BaseSyncTrait
@@ -42,5 +44,43 @@ trait BaseSyncTrait
 
         ];
         return $map;
+    }
+
+    public static function getTableName()
+    {
+        return (new self)->table;
+    }
+
+    public static function insertDataHandle($mdy, $columns, $framework=1)
+    {
+        $data = [];
+        if ($mdy instanceof MingDaoYun) {
+            $_data = $mdy->get();
+        } elseif(isset($mdy['data']['rows'])) {
+            $_data = $mdy;
+        } else {
+            throw new Exception("数据格式有误!");
+        }
+        if ($_data['error_code'] != 1) throw new Exception("获取明道云数据失败!");
+
+        foreach($_data['data']['rows'] as $key => $val) {
+            $arr = [];
+            foreach($columns as $column) {
+                $v = isset($val[$column]) ? $val[$column] : '';
+                if (Filter::getFieldDataType($column) == 14 && !empty($v)) $v = json_decode($v, true)[0]['original_file_full_path'];
+                if (Filter::getFieldDataType($column) == 26 && !empty($v)) $v = $v['fullname'];
+                $arr[$column] = $v;
+            }
+            if ($framework !== 1) {
+                $arr['created_at'] = now();
+                $arr['updated_at'] = now();
+            } else {
+                $arr['created_at'] = date("Y-m-d H:i:s", time());
+            }
+
+            unset($arr['id']);
+            $data[] = $arr;
+        }
+        return $data;
     }
 }
